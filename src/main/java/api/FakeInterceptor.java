fcc7759878d427fb2b581b889978423606e646b3
HichamB <hichamb@engineer.com>
2021-11-12 22:21:37 +0200

    adding passing tests

--- src/main/java/api/FakeInterceptor.java
+++ src/main/java/api/FakeInterceptor.java
@@ -1,15 +1,80 @@
 package api;
 
-import okhttp3.Interceptor;
-import okhttp3.Response;
+import okhttp3.*;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import util.PathDate;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.Arrays;
 
 public class FakeInterceptor implements Interceptor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FakeInterceptor.class);
+    ResponseBody body;
+    String [] validCities = {"lon", "london", "dub", "dubai", "hel", "helsinki", "hyderabad"};
+    LocalDate tomorrow = LocalDate.now().plusDays(1);
+    PathDate pathDate = new PathDate(tomorrow);
+
     @Override
-    public Response intercept(Interceptor.Chain chain) throws IOException {
-        // do not change response in case not matched
-        return chain.proceed(chain.request());
+    public Response intercept(Chain chain) throws IOException {
+        //Request request = chain.request();
+        Response response;
+
+        System.out.println("date: " + pathDate + "/");
+
+        //System.out.println("query: " + chain.request().url().query().split("=")[1]);
+        System.out.println("URi: " + chain.request().url().uri());
+        System.out.println("search endpoint?: " + chain.request().url().uri().getPath().endsWith("location/search/"));
+        System.out.println("forecast endpoint?: " + chain.request().url().uri().getPath().endsWith(pathDate + "/"));
+
+        Boolean search_endpoint = chain.request().url().uri().getPath().endsWith("location/search/");
+        Boolean forecast_endpoint = chain.request().url().uri().getPath().endsWith(pathDate + "/");
+
+        String responseString  = "";
+
+        if (search_endpoint){
+            String searchQuery = chain.request().url().uri().getQuery().split("=")[1];
+            if (!Arrays.asList(validCities).contains(searchQuery)) {
+            responseString = "[]";
+            body = ResponseBody.create("[]".getBytes(StandardCharsets.UTF_8), MediaType.parse("application/json"));
+                //body = ResponseBody.create(Utils.readResourceFileToBytes("badCity.json"), MediaType.parse("application/json"));
+            }
+            else {
+                if ( searchQuery.equals("hyderabad")) {
+                    responseString = "[{\"title\":\"hyderabad\", \"location_type\":\"City\", \"woeid\":99999, \"latt_long\":\"51.506321,-0.12714\"}]";
+                } else{
+                    responseString = "[{\"title\":\"Helsinki\", \"location_type\":\"City\", \"woeid\":44418, \"latt_long\":\"51.506321,-0.12714\"}]";
+                }
+                body = ResponseBody.create("[{\"title\":\"Helsinki\", \"location_type\":\"City\", \"woeid\":44418, \"latt_long\":\"51.506321,-0.12714\"}]".getBytes(StandardCharsets.UTF_8), MediaType.parse("application/json"));
+                //body = ResponseBody.create(Utils.readResourceFileToBytes("fakeCity.json"), MediaType.parse("application/json"));
+            }
+        }
+        else {
+            if (forecast_endpoint){
+                if (chain.request().url().uri().getPath().endsWith("99999/" + pathDate + "/")){
+                    responseString ="[ ]";
+                } else {
+                    responseString ="[ { \"id\": 5749047877959680, \"weather_state_name\": \"Heavy SNOW\", \"weather_state_abbr\": \"hc\", \"wind_direction_compass\": \"WNW\", \"created\": \"2021-11-10T20:35:41.142286Z\", \"applicable_date\": \"2021-11-12\", \"min_temp\": 2.675, \"max_temp\": 6.365, \"the_temp\": 6.265000000000001, \"wind_speed\": 11.226436167870304, \"wind_direction\": 290.16840347962, \"air_pressure\": 1007.5, \"humidity\": 80, \"visibility\": 10.834849479042393, \"predictability\": 71 } ]";
+                }
+
+                body = ResponseBody.create("[ { \"id\": 5749047877959680, \"weather_state_name\": \"Heavy SNOW\", \"weather_state_abbr\": \"hc\", \"wind_direction_compass\": \"WNW\", \"created\": \"2021-11-10T20:35:41.142286Z\", \"applicable_date\": \"2021-11-12\", \"min_temp\": 2.675, \"max_temp\": 6.365, \"the_temp\": 6.265000000000001, \"wind_speed\": 11.226436167870304, \"wind_direction\": 290.16840347962, \"air_pressure\": 1007.5, \"humidity\": 80, \"visibility\": 10.834849479042393, \"predictability\": 71 } ]".getBytes(StandardCharsets.UTF_8), MediaType.parse("application/json"));
+                //body = ResponseBody.create(Utils.readResourceFileToBytes("fakeForcast.json"), MediaType.parse("application/json"));
+            }
+        }
+        response = new Response.Builder()
+                .code(200)
+                .message(body.string())
+                .request(chain.request())
+                .protocol(Protocol.HTTP_1_0)
+                .body(ResponseBody.create(responseString.getBytes(), MediaType.parse("application/json")))
+                //.body(body)   // TODO: fix
+                .addHeader("content-type", "application/json")
+                .build();
+
+        return  response;
+
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,175 @@
+import api.FakeInterceptor;
+import api.ForecastServiceImpl;
+import okhttp3.OkHttpClient;
+import org.junit.After;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import util.PathDate;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
 
-    @Test
-    void testApp() {
+    String[] validCities = {"lon", "london", "dub", "dubai", "hel"};
+    String[] invalidCities = {"Atlantis", "Sokovia", "Wakanda", "123"};
 
-        // test code here
+    Random r=new Random();
+    String validCity;
+    private static ForecastServiceImpl service ;
+    LocalDate tomorrow = LocalDate.now().plusDays(1);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
+
+    ;
+
+
+
+//
+    //private MockWebServer server = new MockWebServer();
+
+    @BeforeAll
+    public static void setup() {
+
+
+        LOGGER.info(() -> "Before Test Setup");
+
+
+        final OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new FakeInterceptor())
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.metaweather.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        service = new ForecastServiceImpl(retrofit);
+
     }
+
+    @After
+    public void teardown() throws IOException {
+        //server.shutdown();
+    }
+
+
+    @Test
+    void testValidCityForecastOutput() throws IOException {
+
+        LOGGER.info(() -> "testValidCityForecastOutput Started");
+
+        try {
+
+            // Given OK city string
+            validCity =  validCities[r.nextInt(validCities.length)];
+
+            // When I call weather api and get a response
+            String responseSting = service.getForecast(validCity,tomorrow);
+            System.out.println(responseSting);
+
+            String [] splits  = responseSting.split("\\R");
+
+
+
+            // Then
+            PathDate pathDate = new PathDate(tomorrow);
+            assertEquals(String.format("Weather on (%s) in Helsinki:", pathDate), splits[0]);
+            // AND
+            assertEquals("Heavy SNOW", splits[1]);
+            // AND
+            assertEquals("Temp: 6.3 °C", splits[2]);
+            // AND
+            assertEquals("Wind: 11.2 mph", splits[3]);
+            // AND
+            assertEquals("Humidity: 80%", splits[4]);
+
+            LOGGER.info(() -> "Test Ended");
+
+
+        } catch (Exception e) {
+            LOGGER.error( () ->  String.format("Retrofit failed with error: %s", e.getMessage()));
+            System.exit(1);
+        }
+
+
+    }
+
+
+
+    @Test
+    void testInvalidCityForecastOutput() throws IOException {
+
+        LOGGER.info(() -> "Test testInvalidCityForecastOutput Started");
+
+        try {
+
+            // Given Invalid city name
+            validCity =  invalidCities[r.nextInt(invalidCities.length)];
+
+            // When I call weather api get a response
+            String responseSting = service.getForecast(validCity,tomorrow);
+            System.out.println(responseSting);
+
+
+
+        } catch (Exception e) {
+            // Then
+            assertEquals(String.format("Can't find city id for '%s'", validCity), e.getMessage());
+            LOGGER.error( () ->  String.format("Retrofit failed with error: %s - Will exit", e.getMessage()));
+            LOGGER.info(() -> "Test testInvalidCityForecastOutput Ended");
+
+
+            // commented out to prevent Exit during dev / testing
+            //System.exit(1);
+        }
+
+
+
+    }
+
+    @Test
+    void testValidCityNoForecast() throws IOException {
+
+        LOGGER.info(() -> "Test testValidCityNoForecast Started");
+
+        try {
+
+
+            // Given valid city name but with bad forecast response
+            validCity =  "hyderabad";
+
+            // When I call weather api get a response
+            String responseSting = service.getForecast(validCity,tomorrow);
+            System.out.println(responseSting);
+
+
+
+        } catch (Exception e) {
+            // Then
+            assertEquals(String.format("Can't get forecast for '%s'", validCity), e.getMessage());
+            LOGGER.error( () ->  String.format("Retrofit failed with error: %s - Will exit", e.getMessage()));
+            LOGGER.info(() -> "Test testValidCityNoForecast Ended");
+
+            // commented out to prevent Exit during dev / testing
+            //System.exit(1);
+        }
+
+
+
+    }
+
+    //Purpoose: Verify basic functioning for the REST API Retrofit app
+    // More tests to come: Status codes, JSON Payload, Media Type, other HTTP response headers in the response
+
+
+
 
 }
